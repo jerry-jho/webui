57235a4d5d0dc4c627857c5692b417db97f02e78
jerry-jho@outlook.com <zhoury@intenginetech.com>
2018-11-27 21:09:34 +0800

    update signals

--- content/control.js
+++ content/control.js
@@ -9,6 +9,11 @@ $(document).ready(function() {
         });   
         $("#btn2").click(function() {
             my_object.saveFile($("#txt2").val(),$("#txt3").val());
-        });         
+        });        
+        var my_timer = channel.objects.MyTimer;
+        my_timer.timeout.connect(function() {
+            $("#ctr0").text(1 + parseInt($("#ctr0").text()));
+        });
+        my_timer.start(2000);
     })
 })
